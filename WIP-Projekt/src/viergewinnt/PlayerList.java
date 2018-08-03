b9b5936982159e38b56936aa0967c5439f4194c9
ssporleder <ssporleder@UHRZ-13713.ad.uni-bielefeld.de>
2018-08-03 16:36:56 +0200

    PlayerList und Player erzeugt

--- WIP-Projekt/src/viergewinnt/PlayerList.java
+++ WIP-Projekt/src/viergewinnt/PlayerList.java
@@ -1,17 +1,23 @@
 package viergewinnt;
 
-
 import java.lang.*;
 import java.util.*;
 
-import spielpaket.*;
+import viergewinnt.*;
 
 public class PlayerList{
 	public int number_of_players;
 	public Map players;
-	
-	//Erzeugt playerlist mit einer HashMap von Players
+
 	public PlayerList(){
 		players = new HashMap();
 	}
+	
+	//Pruft ob Spieler mit Namen name existiert
+	public boolean contact(String name){
+		Player pl = (Player) players.get(name);
+		if (pl == null) {return(true);}
+		else {return(false);}
+	}
+	
 }
--- WIP-Projekt/src/viergewinnt/ServerThread.java
+++ WIP-Projekt/src/viergewinnt/ServerThread.java
@@ -1,15 +1,14 @@
 package viergewinnt;
-
 import java.net.*;
 
-import spielpaket.*;
+import viergewinnt.*;
 
 import java.io.*;
 import java.lang.*;
 
 public class ServerThread extends Thread {
   private Socket socket = null;
-  private SpielProtokoll protocol;
+  //private SpielProtokoll protocol;
   public String name;
   boolean exit = true;
   boolean contact = false;
@@ -21,11 +20,40 @@ public class ServerThread extends Thread {
     this.socket = socket;
     this.playlist = playlist;
     
-    System.out.println("Connection accepted  "
+    System.out.println("Verbindung akzeptiert  "
 	+" for " + socket.getInetAddress()
 	+ ":"+ socket.getPort());
     	
-    protocol = new SpielProtokoll();
+    //protocol = new SpielProtokoll();
   
   }
-}
+  
+  public void run() {
+
+	    try {
+
+	      // Buffer erzeugen
+	      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	      BufferedReader in = new BufferedReader(
+		  new InputStreamReader(socket.getInputStream()));
+
+
+	      String inputLine;
+	      
+	      while(contact == false){ //Schleife die prufen soll ob Spieler existiert
+	        	String name;
+	        	out.println("Bitte geben Sie Ihrem Spielernamen ein: ");    
+	        	name = in.readLine();
+	        	if (playlist.contact(name) == true){
+	        		this.name = name;
+	  		//this.playlist.newplayer(socket.toString(),name);
+	        		
+	        		contact = true;
+	  	}
+	  	else {out.println("This user already exist");}
+	      }
+	    }
+  }
+ 
+  
+  
