0b85ed04560c357442b94b360cb2d9ebfea3573a
Paul <Paul@PSUltrabook>
2018-08-05 13:20:21 +0200

    import verändert. sql.Resultlist nicht mehr benötigt.

--- WIP-Projekt/src/viergewinnt/ServerDatabase.java
+++ WIP-Projekt/src/viergewinnt/ServerDatabase.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 
 public class ServerDatabase {
